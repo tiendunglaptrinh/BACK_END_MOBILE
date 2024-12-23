8a75d81dc21f9926d2c110cfc58212134dd24454
tiendunglaptrinh <dung.takhmtk21@hcmut.edu.vn>
2024-12-23 22:09:34 +0700

    feat: redeploy server - import server postgres

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,14 @@
 import { Sequelize } from '@sequelize/core';
+import "dotenv/config"
 
 const sequelize = new Sequelize({
   dialect: 'postgres', 
-  database: 'roomly',
-  user: 'postgres',
-  password: '13102003',
-  host: 'localhost',
-  port: 8080,
-  ssl: false,
+  database: process.env.DB_NAME,  // Sử dụng biến môi trường DB_NAME
+  user: process.env.DB_USER,  // Sử dụng biến môi trường DB_USER
+  password: process.env.DB_PASSWORD,  // Sử dụng biến môi trường DB_PASSWORD
+  host: process.env.DB_HOST,  // Sử dụng biến môi trường DB_HOST
+  port: process.env.DB_PORT,  // Sử dụng biến môi trường DB_PORT
+  ssl: false,  // Nếu cần SSL (tuỳ vào yêu cầu của Railway, có thể cần cấu hình này)
   clientMinMessages: 'notice',
 });
 
